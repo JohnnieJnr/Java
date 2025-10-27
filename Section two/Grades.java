1f199324e95af6363b5a009f9063475980cb9f43
Johnnie Oduro Jnr <johnnieoduro@gmail.com>
2025-10-27 17:19:44 +0000

    first commit

---
+++ Section two/Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+    public static void main(String[] args) {
+        char potion = 'A';
+        char arithemancy = 'B';
+        char charms = 'C';
+
+        System.out.println("Potions " + potion);
+        System.out.println("Arithmency " + arithemancy);
+        System.out.println("Charms " + charms);
+    }
+}
---
+++ Section two/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int passengers = 5;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.err.println(busTickets);
+
+        int maxValue = 214748364;
+        System.out.println(maxValue);
+    }
+}
